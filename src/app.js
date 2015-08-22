417ef787d1570475a73c812e8123d0b7f13290aa
Kyle Doherty <kyle.l.doherty@gmail.com>
2015-08-22 15:48:47 -0700

    ensure whole app hot reloads

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Component from './component.js'
+
+class App extends React.Component {
+    render() {
+        return <Component />
+    }
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './app'
+    './index'
   ],
   output:  {
     path: __dirname,
