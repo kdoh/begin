1f8337eb708e8cf07d70e925a4f43a9d6c8ffca8
Kyle Doherty <kyle.l.doherty@gmail.com>
2015-12-06 12:16:30 -0800

    add source map support

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'eval-source-map',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
