77622719b74589b3d1892ac7e853191edb30cbf5
Kyle Doherty <kyle.l.doherty@gmail.com>
2016-02-18 16:27:56 -0800

    fix hot reloading

--- src/component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Component = () => {
-  const message = "I'm your component. Have at it."
-  return (<div>{message}</div>)
-}
-
-export default Component
---
+++ src/widget.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Widget extends Component {
+  render () {
+    const message = "I'm your widget. Have at it."
+    return <div>{message}</div>
+  }
+}
+
+export default Widget
